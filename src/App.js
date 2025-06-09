2e2dae21fd9a88c0cebe763cd615bee3ffb3fd95
binuri2018 <imbinuri@gmail.com>
2025-06-09 21:30:37 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/NavBar'
 import About from './components/About'
 import Contact from './components/Contact'
 import Services from './components/Services'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <About />
       <Services />
       <Contact />
+      <Footer />
     </div>
   )
 }
