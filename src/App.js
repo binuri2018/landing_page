181130a98b6ebca542c6b5e99ecc727555139da1
binuri2018 <imbinuri@gmail.com>
2025-06-10 20:12:50 +0530

    blogs

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './components/About'
 import Contact from './components/Contact'
 import Services from './components/Services'
 import Footer from './components/Footer'
+import Blog from './components/Blog'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Hero />
       <About />
       <Services />
+      <Blog />
       <Contact />
       <Footer />
     </div>
