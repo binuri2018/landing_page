cc2433742890972ef655441863de4e3fa9aa3bc3
binuri2018 <imbinuri@gmail.com>
2025-06-11 11:03:44 +0530

    animation changes

--- src/components/Rotatingbox.js
+++ src/components/Rotatingbox.js
@@ -1,28 +1,21 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
-
-
-const Hero = () => {
+const RotatingBox = () => {
   return (
-    <div style={{ height: '100vh', background: '#111' }}>
+    <mesh rotation={[0.5, 0.5, 0]}>
+      <boxGeometry args={[2, 2, 2]} />
+      <meshStandardMaterial color="#4ade80" />
+    </mesh>
+  )
+}
+<div style={{ height: '100vh', background: '#111' }}>
       <Canvas camera={{ position: [5, 5, 5] }}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[2, 2, 2]} intensity={1} />
+        <RotatingBox />
         <OrbitControls enableZoom={false} />
       </Canvas>
-      <div style={{
-        position: 'absolute',
-        top: '20%',
-        left: '10%',
-        color: '#fff',
-        fontSize: '2rem'
-      }}>
-        <h1>Your Company Name</h1>
-        <p>Innovating the Future</p>
-      </div>
     </div>
-  )
-}
 
-export default Hero
+export default RotatingBox;
