56019a9c5163ef1a6cc398e6023395f00712b8e6
binuri2018 <imbinuri@gmail.com>
2025-06-11 11:06:43 +0530

    animation changes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,6 +2,14 @@ import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 
+const RotatingBox = () => {
+  return (
+    <mesh rotation={[0.5, 0.5, 0]}>
+      <boxGeometry args={[2, 2, 2]} />
+      <meshStandardMaterial color="#4ade80" />
+    </mesh>
+  )
+}
 
 const Hero = () => {
   return (
@@ -9,6 +17,7 @@ const Hero = () => {
       <Canvas camera={{ position: [5, 5, 5] }}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[2, 2, 2]} intensity={1} />
+        <RotatingBox />
         <OrbitControls enableZoom={false} />
       </Canvas>
       <div style={{
