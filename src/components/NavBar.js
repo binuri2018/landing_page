9f7e4f666b6e8fc8e02bb4b87d70b6b3f7fe81a4
binuri2018 <imbinuri@gmail.com>
2025-06-11 10:56:43 +0530

    logo added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import styles from './NavBar.module.css'
+import logo from '../Assets/logo.png' // Adjust path as needed
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-      <div className={styles.logo}>YourLogo</div>
+      <div className={styles.logo}>
+        <img src={logo} alt="BlackWhite Studio Logo" className={styles.logoImage} />
+      </div>
       <ul className={styles.navLinks}>
         <li><a href="#about">About</a></li>
         <li><a href="#services">Services</a></li>
